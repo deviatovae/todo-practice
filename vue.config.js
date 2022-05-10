4b7e23018bd01f888a1be72010a43e576a02a433
Alena Deviatova <www.fidgetl@gmail.com>
2022-05-10 18:52:15 +0300

    configure vue build config for github, commit static

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'github' ? '/todo-practice/' : '/',
+  outputDir: path.resolve(__dirname, process.env.NODE_ENV === 'github' ? "./docs" : "./dist"),
 })
